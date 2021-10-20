c42c659ab5a1cdec88ff8feeeb489e0a3b921661
Alex <psihicus@gmail.com>
2021-10-20 18:11:19 +0300

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,47 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+import static java.time.format.DateTimeFormatter.*;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+
+    private static final Map<String, String> MONTHS = Map.ofEntries(
+            new AbstractMap.SimpleEntry<>("янв", "01"),
+            new AbstractMap.SimpleEntry<>("фев", "02"),
+            new AbstractMap.SimpleEntry<>("мар", "03"),
+            new AbstractMap.SimpleEntry<>("апр", "04"),
+            new AbstractMap.SimpleEntry<>("май", "05"),
+            new AbstractMap.SimpleEntry<>("июн", "06"),
+            new AbstractMap.SimpleEntry<>("июл", "07"),
+            new AbstractMap.SimpleEntry<>("авг", "08"),
+            new AbstractMap.SimpleEntry<>("сен", "09"),
+            new AbstractMap.SimpleEntry<>("окт", "10"),
+            new AbstractMap.SimpleEntry<>("ноя", "11"),
+            new AbstractMap.SimpleEntry<>("дек", "12")
+            );
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MM yy HH:mm");
+                parse = parse.replace(",", "");
+        if (parse.contains("сегодня")) {
+            parse=parse.replace("сегодня", LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd MM yy")));
+
+        } else  if(parse.contains("вчера")) {
+            parse=parse.replace("вчера", LocalDateTime.now().minusDays(1).format(DateTimeFormatter.ofPattern("dd MM yy")));
+        }
+        else {
+
+            String[] tmp = parse.split(" ");
+            tmp[1] = MONTHS.get(tmp[1]);
+            parse = String.join(" ", tmp);
+        }
+
+        return LocalDateTime.parse(parse, formatter);
+    }
+}
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,15 +4,19 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".icon_cell");
+        Elements row = doc.select(".postslisttopic");
         for (Element td : row) {
             Element element = td.parent().children().get(5);
-            System.out.println(element.text());
+            DateTimeParser dateTimeParser = new SqlRuDateTimeParser();
+            if (!td.parent().children().get(5).hasAttr("Текст")) {
+                System.out.println(dateTimeParser.parse(element.text()));
+            }
         }
-
     }
 }
