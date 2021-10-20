d6c12fd4551610c241edcd967a50ae5ea9df8fa4
Alex <psihicus@gmail.com>
2021-10-20 15:07:45 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,18 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".icon_cell");
+        for (Element td : row) {
+            Element element = td.parent().children().get(5);
+            System.out.println(element.text());
+        }
+
+    }
+}
