888142f4dff5e4c64462302210c91a7753c4f760
Alex <psihicus@gmail.com>
2021-10-20 18:24:11 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -9,13 +9,15 @@ import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element element = td.parent().children().get(5);
-            DateTimeParser dateTimeParser = new SqlRuDateTimeParser();
-            if (!td.parent().children().get(5).hasAttr("Текст")) {
-                System.out.println(dateTimeParser.parse(element.text()));
+        for (int i = 1; i <= 5; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element element = td.parent().children().get(5);
+                DateTimeParser dateTimeParser = new SqlRuDateTimeParser();
+                if (!td.parent().children().get(5).hasAttr("Текст")) {
+                    System.out.println(dateTimeParser.parse(element.text()));
+                }
             }
         }
     }
