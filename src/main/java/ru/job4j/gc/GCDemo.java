46c7740c73bd076e7d61ef536ae37855d164ccc7
Alex <psihicus@gmail.com>
2021-10-25 18:48:25 +0300

    1. Демонстрация работы GC [#1589]

---
+++ src/main/java/ru/job4j/gc/GCDemo.java
@@ -0,0 +1,26 @@
+package ru.job4j.gc;
+
+public class GCDemo {
+
+    private static final long KB = 1000;
+    private static final long MB = KB * KB;
+    private static final Runtime ENVIRONMENT = Runtime.getRuntime();
+
+    public static void info() {
+        final long freeMemory = ENVIRONMENT.freeMemory();
+        final long totalMemory = ENVIRONMENT.totalMemory();
+        final long maxMemory = ENVIRONMENT.maxMemory();
+        System.out.println("=== Environment state ===");
+        System.out.printf("Free: %d%n", freeMemory / KB);
+        System.out.printf("Total: %d%n", totalMemory / KB);
+        System.out.printf("Max: %d%n", maxMemory / KB);
+    }
+
+    public static void main(String[] args) {
+        info();
+        for (int i = 0; i < 5000; i++) {
+            new User(i, "a");
+        }
+        info();
+    }
+}
