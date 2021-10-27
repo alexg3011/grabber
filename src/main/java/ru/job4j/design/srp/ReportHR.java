6f2d6e6f61e6545148ce3d053afafc3dacb9ae0e
Alex <psihicus@gmail.com>
2021-10-27 19:41:28 +0300

    1. Отчеты. [#850]

---
+++ src/main/java/ru/job4j/design/srp/ReportHR.java
@@ -0,0 +1,28 @@
+package ru.job4j.design.srp;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ReportHR implements Report {
+
+    private Store store;
+
+    public ReportHR(Store store) {
+        this.store = store;
+    }
+
+    @Override
+    public String generate(Predicate<Employee> filter) {
+        StringBuilder text = new StringBuilder();
+        List<Employee> employees = store.findBy(filter);
+        employees.sort((x1, x2) -> (int) (x2.getSalary() - x1.getSalary()));
+        text.append("Name; Salary;")
+                .append(System.lineSeparator());
+        for (Employee employee : employees) {
+            text.append(employee.getName()).append(";")
+                    .append(employee.getSalary()).append(";")
+                    .append(System.lineSeparator());
+        }
+        return text.toString();
+    }
+}
