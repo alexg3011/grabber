783342fd19bf2119d25003389718c55c0be4f662
Alex <psihicus@gmail.com>
2021-11-01 12:21:00 +0300

    1. Хранилище продуктов [#852 #221761]

--- src/main/java/ru/job4j/ood/lsp/ControlQuality.java
+++ src/main/java/ru/job4j/ood/lsp/ControlQuality.java
@@ -3,7 +3,15 @@ package ru.job4j.ood.lsp;
 import java.util.List;
 
 public class ControlQuality {
+    private List<Storage> storages;
 
-    public ControlQuality(List<Storage> list) {
+    public ControlQuality(List<Storage> storages) {
+        this.storages = storages;
+    }
+
+    public void distribute(Food food) {
+        for (Storage storage : storages) {
+            storage.add(food);
+        }
     }
 }
